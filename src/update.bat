stahta01/codeblocks_https_metadata

commit fa58a0902391e92a832105716662cea98d20a23b

- build: Unify update*.bat (ticket #816, thanks Miguel Gimenez)
> Copied update31_64.bat over update.bat, extended the generalization and
  modified the other batch files, so they call update.bat with the version
  as a parameter.
> Also, got rid of the logic surrounding the copy/rename of
  cb_console_runner.exe using copy instead of xcopy, as it can copy with a
  new name and will replace the file if it existed.

git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11652 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/update.bat
@@ -3,48 +3,51 @@
 REM SETLOCAL assures environment variables created in a batch file are not exported to its calling environment
 setlocal
 
+SETLOCAL ENABLEEXTENSIONS
+
 echo Creating output directory tree
 
-set CB_DEVEL_RESDIR=devel\share\CodeBlocks
-set CB_OUTPUT_RESDIR=output\share\CodeBlocks
+set CB_DEVEL_DIR=devel%1%
+set CB_OUTPUT_DIR=output%1%
+set CB_DEVEL_RESDIR=%CB_DEVEL_DIR%\share\CodeBlocks
+set CB_OUTPUT_RESDIR=%CB_OUTPUT_DIR%\share\CodeBlocks
+set CB_HANDLER_DIR=exchndl\win32\bin
+set %i% | find "_64" > nul && set CB_HANDLER_DIR=exchndl\win64\bin
 
-if not exist output md output\
-if not exist output\share md output\share\
-if not exist %CB_OUTPUT_RESDIR% md %CB_OUTPUT_RESDIR%\
-if not exist %CB_OUTPUT_RESDIR%\compilers md %CB_OUTPUT_RESDIR%\compilers\
-if not exist %CB_OUTPUT_RESDIR%\lexers md %CB_OUTPUT_RESDIR%\lexers\
-if not exist %CB_OUTPUT_RESDIR%\images md %CB_OUTPUT_RESDIR%\images\
-if not exist %CB_OUTPUT_RESDIR%\images\settings md %CB_OUTPUT_RESDIR%\images\settings\
-if not exist %CB_OUTPUT_RESDIR%\images\16x16 md %CB_OUTPUT_RESDIR%\images\16x16\
-if not exist %CB_OUTPUT_RESDIR%\images\22x22 md %CB_OUTPUT_RESDIR%\images\22x22\
-if not exist %CB_OUTPUT_RESDIR%\images\codecompletion md %CB_OUTPUT_RESDIR%\images\codecompletion\
-if not exist %CB_OUTPUT_RESDIR%\plugins md %CB_OUTPUT_RESDIR%\plugins\
-if not exist %CB_OUTPUT_RESDIR%\templates md %CB_OUTPUT_RESDIR%\templates\
-if not exist %CB_OUTPUT_RESDIR%\templates\wizard md %CB_OUTPUT_RESDIR%\templates\wizard\
-if not exist %CB_OUTPUT_RESDIR%\scripts md %CB_OUTPUT_RESDIR%\scripts\
-if not exist %CB_OUTPUT_RESDIR%\scripts\tests md %CB_OUTPUT_RESDIR%\scripts\tests\
-if not exist devel md devel\
-if not exist devel\share md devel\share\
-if not exist %CB_DEVEL_RESDIR% md %CB_DEVEL_RESDIR%\
-if not exist %CB_DEVEL_RESDIR%\compilers md %CB_DEVEL_RESDIR%\compilers\
-if not exist %CB_DEVEL_RESDIR%\lexers md %CB_DEVEL_RESDIR%\lexers\
-if not exist %CB_DEVEL_RESDIR%\images md %CB_DEVEL_RESDIR%\images\
-if not exist %CB_DEVEL_RESDIR%\images\settings md %CB_DEVEL_RESDIR%\images\settings\
-if not exist %CB_DEVEL_RESDIR%\images\16x16 md %CB_DEVEL_RESDIR%\images\16x16\
-if not exist %CB_DEVEL_RESDIR%\images\22x22 md %CB_DEVEL_RESDIR%\images\22x22\
-if not exist %CB_DEVEL_RESDIR%\images\codecompletion md %CB_DEVEL_RESDIR%\images\codecompletion\
-if not exist %CB_DEVEL_RESDIR%\plugins md %CB_DEVEL_RESDIR%\plugins\
-if not exist %CB_DEVEL_RESDIR%\templates md %CB_DEVEL_RESDIR%\templates\
-if not exist %CB_DEVEL_RESDIR%\templates\wizard md %CB_DEVEL_RESDIR%\templates\wizard\
-if not exist %CB_DEVEL_RESDIR%\scripts md %CB_DEVEL_RESDIR%\scripts\
-if not exist %CB_DEVEL_RESDIR%\scripts\tests md %CB_DEVEL_RESDIR%\scripts\tests\
+call:mkdirSilent %CB_DEVEL_RESDIR%\compilers
+call:mkdirSilent %CB_DEVEL_RESDIR%\lexers
+call:mkdirSilent %CB_DEVEL_RESDIR%\images\settings
+call:mkdirSilent %CB_DEVEL_RESDIR%\images\codecompletion
+call:mkdirSilent %CB_DEVEL_RESDIR%\images\12x12
+call:mkdirSilent %CB_DEVEL_RESDIR%\images\16x16
+call:mkdirSilent %CB_DEVEL_RESDIR%\images\22x22
+call:mkdirSilent %CB_DEVEL_RESDIR%\images\32x32
+call:mkdirSilent %CB_DEVEL_RESDIR%\plugins
+call:mkdirSilent %CB_DEVEL_RESDIR%\templates
+call:mkdirSilent %CB_DEVEL_RESDIR%\templates\wizard
+call:mkdirSilent %CB_DEVEL_RESDIR%\scripts\tests
+
+call:mkdirSilent %CB_OUTPUT_RESDIR%\compilers
+call:mkdirSilent %CB_OUTPUT_RESDIR%\lexers
+call:mkdirSilent %CB_OUTPUT_RESDIR%\images\settings
+call:mkdirSilent %CB_OUTPUT_RESDIR%\images\codecompletion
+call:mkdirSilent %CB_OUTPUT_RESDIR%\images\12x12
+call:mkdirSilent %CB_OUTPUT_RESDIR%\images\16x16
+call:mkdirSilent %CB_OUTPUT_RESDIR%\images\22x22
+call:mkdirSilent %CB_OUTPUT_RESDIR%\images\32x32
+call:mkdirSilent %CB_OUTPUT_RESDIR%\plugins
+call:mkdirSilent %CB_OUTPUT_RESDIR%\templates
+call:mkdirSilent %CB_OUTPUT_RESDIR%\templates\wizard
+call:mkdirSilent %CB_OUTPUT_RESDIR%\scripts\tests
+
+rem exit /b
 
 set ZIPCMD=zip
 
 echo Compressing core UI resources
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\resources.zip src\resources\*.xrc > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\manager_resources.zip sdk\resources\*.xrc sdk\resources\images\*.png > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\start_here.zip src\resources\start_here\*.html src\resources\start_here\*.png > nul
+%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\start_here.zip src\resources\start_here\* > nul
 echo Compressing plugins UI resources
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\Astyle.zip plugins\astyle\resources\manifest.xml plugins\astyle\resources\*.xrc > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\autosave.zip plugins\autosave\manifest.xml plugins\autosave\*.xrc > nul
@@ -55,69 +58,133 @@ echo Compressing plugins UI resources
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\defaultmimehandler.zip plugins\defaultmimehandler\resources\manifest.xml plugins\defaultmimehandler\resources\*.xrc > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\occurrenceshighlighting.zip plugins\occurrenceshighlighting\resources\*.xrc plugins\occurrenceshighlighting\resources\manifest.xml > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\openfileslist.zip plugins\openfileslist\manifest.xml > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\projectsimporter.zip plugins\projectsimporter\resources\manifest.xml plugins\projectsimporter\resources\*.xrc > nul
+%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\projectsimporter.zip plugins\projectsimporter\resources\*.xrc plugins\projectsimporter\resources\manifest.xml > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\scriptedwizard.zip plugins\scriptedwizard\resources\manifest.xml > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\todo.zip plugins\todo\resources\manifest.xml plugins\todo\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\xpmanifest.zip plugins\xpmanifest\manifest.xml > nul
 %ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\abbreviations.zip plugins\abbreviations\resources\manifest.xml plugins\abbreviations\resources\*.xrc > nul
+%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\xpmanifest.zip plugins\xpmanifest\manifest.xml > nul
 echo Packing core UI bitmaps
 cd src\resources
 %ZIPCMD% -0 -qu ..\..\%CB_DEVEL_RESDIR%\resources.zip images\*.png images\12x12\*.png images\16x16\*.png images\22x22\*.png  images\32x32\*.png> nul
 cd ..\..\sdk\resources
-%ZIPCMD% -0 -qu ..\..\%CB_DEVEL_RESDIR%\manager_resources.zip images\*.png images\12x12\*.png images\16x16\*.png images\22x22\*.png > nul
+%ZIPCMD% -0 -qu ..\..\%CB_DEVEL_RESDIR%\manager_resources.zip images\*.png images\12x12\*.png images\16x16\*.png > nul
 echo Packing plugins UI bitmaps
 cd ..\..\plugins\compilergcc\resources
 %ZIPCMD% -0 -qu ..\..\..\%CB_DEVEL_RESDIR%\compiler.zip images\16x16\*.png images\22x22\*.png images\32x32\*.png > nul
-cd ..\..\..\plugins\debuggergdb\resources
-%ZIPCMD% -0 -qu ..\..\..\%CB_DEVEL_RESDIR%\debugger.zip images\*.png images\16x16\*.png images\22x22\*.png > nul
 cd ..\..\..
 
 echo Copying files
-xcopy /D /y %CB_DEVEL_RESDIR%\*.zip %CB_OUTPUT_RESDIR% > nul
+REM  Create an exclude pattern file
+echo \.svn\      > excludes%1%.txt
+echo Makefile >> excludes%1%.txt
+echo Makefile.am >> excludes%1%.txt
+echo Makefile.in >> excludes%1%.txt
+
 xcopy /D /y sdk\resources\lexers\lexer_* %CB_DEVEL_RESDIR%\lexers > nul
-xcopy /D /y sdk\resources\lexers\lexer_* %CB_OUTPUT_RESDIR%\lexers > nul
 xcopy /D /y src\resources\images\*.png %CB_DEVEL_RESDIR%\images > nul
 xcopy /D /y src\resources\images\settings\*.png %CB_DEVEL_RESDIR%\images\settings > nul
+xcopy /D /y src\resources\images\16x16\*.png %CB_DEVEL_RESDIR%\images\16x16 > nul
+xcopy /D /y src\resources\images\22x22\*.png %CB_DEVEL_RESDIR%\images\22x22 > nul
+xcopy /D /y src\resources\images\32x32\*.png %CB_DEVEL_RESDIR%\images\32x32 > nul
+xcopy /D /y plugins\codecompletion\resources\images\*.png %CB_DEVEL_RESDIR%\images\codecompletion > nul
+xcopy /D /y plugins\compilergcc\resources\compilers\*.xml %CB_DEVEL_RESDIR%\compilers > nul
+xcopy /D /y /s plugins\scriptedwizard\resources\* %CB_DEVEL_RESDIR%\templates\wizard /EXCLUDE:excludes%1%.txt >nul
+xcopy /D /y templates\common\* %CB_DEVEL_RESDIR%\templates /EXCLUDE:excludes%1%.txt > nul
+xcopy /D /y templates\win32\* %CB_DEVEL_RESDIR%\templates /EXCLUDE:excludes%1%.txt > nul
+xcopy /D /y scripts\* %CB_DEVEL_RESDIR%\scripts /EXCLUDE:excludes%1%.txt > nul
+xcopy /D /y scripts\tests\* %CB_DEVEL_RESDIR%\scripts\tests /EXCLUDE:excludes%1%.txt > nul
+
+xcopy /D /y %CB_DEVEL_RESDIR%\*.zip %CB_OUTPUT_RESDIR% > nul
+xcopy /D /y sdk\resources\lexers\lexer_* %CB_OUTPUT_RESDIR%\lexers > nul
 xcopy /D /y src\resources\images\*.png %CB_OUTPUT_RESDIR%\images > nul
 xcopy /D /y src\resources\images\settings\*.png %CB_OUTPUT_RESDIR%\images\settings > nul
-xcopy /D /y src\resources\images\16x16\*.png %CB_DEVEL_RESDIR%\images\16x16 > nul
 xcopy /D /y src\resources\images\16x16\*.png %CB_OUTPUT_RESDIR%\images\16x16 > nul
-xcopy /D /y src\resources\images\22x22\*.png %CB_DEVEL_RESDIR%\images\22x22 > nul
 xcopy /D /y src\resources\images\22x22\*.png %CB_OUTPUT_RESDIR%\images\22x22 > nul
-xcopy /D /y plugins\codecompletion\resources\images\*.png %CB_DEVEL_RESDIR%\images\codecompletion > nul
+xcopy /D /y src\resources\images\32x32\*.png %CB_OUTPUT_RESDIR%\images\32x32 > nul
 xcopy /D /y plugins\codecompletion\resources\images\*.png %CB_OUTPUT_RESDIR%\images\codecompletion > nul
-xcopy /D /y plugins\compilergcc\resources\compilers\*.xml %CB_DEVEL_RESDIR%\compilers > nul
 xcopy /D /y plugins\compilergcc\resources\compilers\*.xml %CB_OUTPUT_RESDIR%\compilers > nul
-echo Makefile.am >  excludes.txt
-echo Makefile.in >> excludes.txt
-echo \.svn\      >> excludes.txt
-xcopy /D /y /s plugins\scriptedwizard\resources\* %CB_DEVEL_RESDIR%\templates\wizard /EXCLUDE:excludes.txt >nul
-xcopy /D /y /s plugins\scriptedwizard\resources\* %CB_OUTPUT_RESDIR%\templates\wizard /EXCLUDE:excludes.txt >nul
-xcopy /D /y templates\common\* %CB_OUTPUT_RESDIR%\templates /EXCLUDE:excludes.txt > nul
-xcopy /D /y templates\win32\* %CB_OUTPUT_RESDIR%\templates /EXCLUDE:excludes.txt > nul
-xcopy /D /y templates\common\* %CB_DEVEL_RESDIR%\templates /EXCLUDE:excludes.txt > nul
-xcopy /D /y templates\win32\* %CB_DEVEL_RESDIR%\templates /EXCLUDE:excludes.txt > nul
-xcopy /D /y scripts\* %CB_DEVEL_RESDIR%\scripts /EXCLUDE:excludes.txt > nul
-xcopy /D /y scripts\* %CB_OUTPUT_RESDIR%\scripts /EXCLUDE:excludes.txt > nul
-xcopy /D /y scripts\tests\* %CB_DEVEL_RESDIR%\scripts\tests /EXCLUDE:excludes.txt > nul
-xcopy /D /y scripts\tests\* %CB_OUTPUT_RESDIR%\scripts\tests /EXCLUDE:excludes.txt > nul
-del excludes.txt
-xcopy /D /y tips.txt %CB_DEVEL_RESDIR% > nul
-xcopy /D /y tips.txt %CB_OUTPUT_RESDIR% > nul
-xcopy /D /y tools\ConsoleRunner\cb_console_runner.exe output > nul
-xcopy /D /y tools\ConsoleRunner\cb_console_runner.exe devel > nul
-xcopy /D /y devel\*.exe output > nul
-xcopy /D /y devel\*.dll output > nul
+xcopy /D /y /s plugins\scriptedwizard\resources\* %CB_OUTPUT_RESDIR%\templates\wizard /EXCLUDE:excludes%1%.txt >nul
+xcopy /D /y templates\common\* %CB_OUTPUT_RESDIR%\templates /EXCLUDE:excludes%1%.txt > nul
+xcopy /D /y templates\win32\* %CB_OUTPUT_RESDIR%\templates /EXCLUDE:excludes%1%.txt > nul
+xcopy /D /y scripts\tests\* %CB_OUTPUT_RESDIR%\scripts\tests /EXCLUDE:excludes%1%.txt > nul
+xcopy /D /y scripts\* %CB_OUTPUT_RESDIR%\scripts /EXCLUDE:excludes%1%.txt > nul
+
+del excludes%1%.txt
+
+REM several contrib plugins
+if exist %CB_DEVEL_RESDIR%\images\codesnippets (
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\codesnippets
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\codesnippets\*.png %CB_OUTPUT_RESDIR%\images\codesnippets > nul
+)
+
+if exist %CB_DEVEL_RESDIR%\images\DoxyBlocks (
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\DoxyBlocks\32x32
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\DoxyBlocks\32x32\*.png %CB_OUTPUT_RESDIR%\images\DoxyBlocks/32x32 > nul
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\DoxyBlocks\22x22
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\DoxyBlocks\22x22\*.png %CB_OUTPUT_RESDIR%\images\DoxyBlocks/22x22 > nul
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\DoxyBlocks\16x16
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\DoxyBlocks\16x16\*.png %CB_OUTPUT_RESDIR%\images\DoxyBlocks/16x16 > nul
+)
+
+if exist %CB_DEVEL_RESDIR%\images\ThreadSearch (
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\ThreadSearch\32x32
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\ThreadSearch\32x32\*.png %CB_OUTPUT_RESDIR%\images\ThreadSearch/32x32 > nul
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\ThreadSearch\22x22
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\ThreadSearch\22x22\*.png %CB_OUTPUT_RESDIR%\images\ThreadSearch/22x22 > nul
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\ThreadSearch\16x16
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\ThreadSearch\16x16\*.png %CB_OUTPUT_RESDIR%\images\ThreadSearch/16x16 > nul
+)
+
+if exist %CB_DEVEL_RESDIR%\images\wxsmith (
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\images\wxsmith
+    xcopy /D /y %CB_DEVEL_RESDIR%\images\wxsmith\*.png %CB_OUTPUT_RESDIR%\images\wxsmith > nul
+)
+
+if exist %CB_DEVEL_RESDIR%\lib_finder (
+    call:mkdirSilent %CB_OUTPUT_RESDIR%\lib_finder
+    xcopy /D /y %CB_DEVEL_RESDIR%\lib_finder\*.xml %CB_OUTPUT_RESDIR%\lib_finder > nul
+)
+
+REM misc. contrib plugin settings:
+xcopy /D /y %CB_DEVEL_RESDIR%\images\16x16\*.png    %CB_OUTPUT_RESDIR%\images\16x16    > nul
+xcopy /D /y %CB_DEVEL_RESDIR%\images\22x22\*.png    %CB_OUTPUT_RESDIR%\images\22x22    > nul
+xcopy /D /y %CB_DEVEL_RESDIR%\images\32x32\*.png    %CB_OUTPUT_RESDIR%\images\32x32    > nul
+xcopy /D /y %CB_DEVEL_RESDIR%\images\settings\*.png %CB_OUTPUT_RESDIR%\images\settings > nul
+
+REM =============================================
+REM =============================================
+REM =============================================
+REM =============================================
+
+copy tips.txt %CB_DEVEL_RESDIR% > nul
+copy tools\ConsoleRunner\cb_console_runner%1%.exe %CB_DEVEL_DIR%\cb_console_runner.exe > nul
+
+copy tips.txt %CB_OUTPUT_RESDIR% > nul
+copy tools\ConsoleRunner\cb_console_runner%1%.exe %CB_OUTPUT_DIR%\cb_console_runner.exe > nul
+
+xcopy /D /y %CB_DEVEL_DIR%\*.exe %CB_OUTPUT_DIR% > nul
+xcopy /D /y %CB_DEVEL_DIR%\*.dll %CB_OUTPUT_DIR% > nul
 xcopy /D /y %CB_DEVEL_RESDIR%\plugins\*.dll %CB_OUTPUT_RESDIR%\plugins > nul
 
 echo Stripping debug info from output tree
-strip output\*.exe
-strip output\*.dll
+strip %CB_OUTPUT_DIR%\*.exe
+strip %CB_OUTPUT_DIR%\*.dll
 strip %CB_OUTPUT_RESDIR%\plugins\*.dll
 
 REM Copy these files later as stripping symbols would corrupt them
 echo Copying crash handler files
-xcopy /y exchndl\win32\bin\*.dll devel  > nul
-xcopy /y exchndl\win32\bin\*.yes devel  > nul
-xcopy /y exchndl\win32\bin\*.dll output > nul
-xcopy /y exchndl\win32\bin\*.yes output > nul
+xcopy /y %CB_HANDLER_DIR%\*.dll %CB_DEVEL_DIR%  > nul
+xcopy /y %CB_HANDLER_DIR%\*.yes %CB_DEVEL_DIR%  > nul
+xcopy /y %CB_HANDLER_DIR%\*.dll %CB_OUTPUT_DIR% > nul
+xcopy /y %CB_HANDLER_DIR%\*.yes %CB_OUTPUT_DIR% > nul
+
+
+::--------------------------------------------------------
+::-- Function section starts below here
+::--------------------------------------------------------
+GOTO:EOF
+
+:mkdirSilent - create a directory if it doesn't exists
+echo make dir %~1
+if not exist "%~1" mkdir "%~1"
+GOTO:EOF

src/update30.bat
@@ -1,124 +1,1 @@
-@echo off
-
-REM SETLOCAL assures environment variables created in a batch file are not exported to its calling environment
-setlocal
-
-echo Creating output30 directory tree
-
-set CB_DEVEL_RESDIR=devel30\share\CodeBlocks
-set CB_OUTPUT_RESDIR=output30\share\CodeBlocks
-
-if not exist output30 md output30\
-if not exist output30\share md output30\share\
-if not exist %CB_OUTPUT_RESDIR% md %CB_OUTPUT_RESDIR%\
-if not exist %CB_OUTPUT_RESDIR%\compilers md %CB_OUTPUT_RESDIR%\compilers\
-if not exist %CB_OUTPUT_RESDIR%\lexers md %CB_OUTPUT_RESDIR%\lexers\
-if not exist %CB_OUTPUT_RESDIR%\images md %CB_OUTPUT_RESDIR%\images\
-if not exist %CB_OUTPUT_RESDIR%\images\settings md %CB_OUTPUT_RESDIR%\images\settings\
-if not exist %CB_OUTPUT_RESDIR%\images\16x16 md %CB_OUTPUT_RESDIR%\images\16x16\
-if not exist %CB_OUTPUT_RESDIR%\images\22x22 md %CB_OUTPUT_RESDIR%\images\22x22\
-if not exist %CB_OUTPUT_RESDIR%\images\codecompletion md %CB_OUTPUT_RESDIR%\images\codecompletion\
-if not exist %CB_OUTPUT_RESDIR%\plugins md %CB_OUTPUT_RESDIR%\plugins\
-if not exist %CB_OUTPUT_RESDIR%\templates md %CB_OUTPUT_RESDIR%\templates\
-if not exist %CB_OUTPUT_RESDIR%\templates\wizard md %CB_OUTPUT_RESDIR%\templates\wizard\
-if not exist %CB_OUTPUT_RESDIR%\scripts md %CB_OUTPUT_RESDIR%\scripts\
-if not exist %CB_OUTPUT_RESDIR%\scripts\tests md %CB_OUTPUT_RESDIR%\scripts\tests\
-if not exist devel30 md devel30\
-if not exist devel30\share md devel30\share\
-if not exist %CB_DEVEL_RESDIR% md %CB_DEVEL_RESDIR%\
-if not exist %CB_DEVEL_RESDIR%\compilers md %CB_DEVEL_RESDIR%\compilers\
-if not exist %CB_DEVEL_RESDIR%\lexers md %CB_DEVEL_RESDIR%\lexers\
-if not exist %CB_DEVEL_RESDIR%\images md %CB_DEVEL_RESDIR%\images\
-if not exist %CB_DEVEL_RESDIR%\images\settings md %CB_DEVEL_RESDIR%\images\settings\
-if not exist %CB_DEVEL_RESDIR%\images\16x16 md %CB_DEVEL_RESDIR%\images\16x16\
-if not exist %CB_DEVEL_RESDIR%\images\22x22 md %CB_DEVEL_RESDIR%\images\22x22\
-if not exist %CB_DEVEL_RESDIR%\images\codecompletion md %CB_DEVEL_RESDIR%\images\codecompletion\
-if not exist %CB_DEVEL_RESDIR%\plugins md %CB_DEVEL_RESDIR%\plugins\
-if not exist %CB_DEVEL_RESDIR%\templates md %CB_DEVEL_RESDIR%\templates\
-if not exist %CB_DEVEL_RESDIR%\templates\wizard md %CB_DEVEL_RESDIR%\templates\wizard\
-if not exist %CB_DEVEL_RESDIR%\scripts md %CB_DEVEL_RESDIR%\scripts\
-if not exist %CB_DEVEL_RESDIR%\scripts\tests md %CB_DEVEL_RESDIR%\scripts\tests\
-
-set ZIPCMD=zip
-
-echo Compressing core UI resources
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\resources.zip src\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\manager_resources.zip sdk\resources\*.xrc sdk\resources\images\*.png > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\start_here.zip src\resources\start_here\*.html src\resources\start_here\*.png > nul
-echo Compressing plugins UI resources
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\Astyle.zip plugins\astyle\resources\manifest.xml plugins\astyle\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\autosave.zip plugins\autosave\manifest.xml plugins\autosave\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\classwizard.zip plugins\classwizard\resources\manifest.xml plugins\classwizard\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\codecompletion.zip plugins\codecompletion\resources\manifest.xml plugins\codecompletion\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\compiler.zip plugins\compilergcc\resources\manifest.xml plugins\compilergcc\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\debugger.zip plugins\debuggergdb\resources\manifest.xml plugins\debuggergdb\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\defaultmimehandler.zip plugins\defaultmimehandler\resources\manifest.xml plugins\defaultmimehandler\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\occurrenceshighlighting.zip plugins\occurrenceshighlighting\resources\*.xrc plugins\occurrenceshighlighting\resources\manifest.xml > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\openfileslist.zip plugins\openfileslist\manifest.xml > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\projectsimporter.zip plugins\projectsimporter\resources\manifest.xml plugins\projectsimporter\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\scriptedwizard.zip plugins\scriptedwizard\resources\manifest.xml > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\todo.zip plugins\todo\resources\manifest.xml plugins\todo\resources\*.xrc > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\xpmanifest.zip plugins\xpmanifest\manifest.xml > nul
-%ZIPCMD% -jqu9 %CB_DEVEL_RESDIR%\abbreviations.zip plugins\abbreviations\resources\manifest.xml plugins\abbreviations\resources\*.xrc > nul
-echo Packing core UI bitmaps
-cd src\resources
-%ZIPCMD% -0 -qu ..\..\%CB_DEVEL_RESDIR%\resources.zip images\*.png images\12x12\*.png images\16x16\*.png images\22x22\*.png  images\32x32\*.png> nul
-cd ..\..\sdk\resources
-%ZIPCMD% -0 -qu ..\..\%CB_DEVEL_RESDIR%\manager_resources.zip images\*.png images\12x12\*.png images\16x16\*.png images\22x22\*.png > nul
-echo Packing plugins UI bitmaps
-cd ..\..\plugins\compilergcc\resources
-%ZIPCMD% -0 -qu ..\..\..\%CB_DEVEL_RESDIR%\compiler.zip images\16x16\*.png images\22x22\*.png images\32x32\*.png > nul
-cd ..\..\..\plugins\debuggergdb\resources
-%ZIPCMD% -0 -qu ..\..\..\%CB_DEVEL_RESDIR%\debugger.zip images\*.png images\16x16\*.png images\22x22\*.png > nul
-cd ..\..\..
-
-echo Copying files
-xcopy /D /y %CB_DEVEL_RESDIR%\*.zip %CB_OUTPUT_RESDIR% > nul
-xcopy /D /y sdk\resources\lexers\lexer_* %CB_DEVEL_RESDIR%\lexers > nul
-xcopy /D /y sdk\resources\lexers\lexer_* %CB_OUTPUT_RESDIR%\lexers > nul
-xcopy /D /y src\resources\images\*.png %CB_DEVEL_RESDIR%\images > nul
-xcopy /D /y src\resources\images\settings\*.png %CB_DEVEL_RESDIR%\images\settings > nul
-xcopy /D /y src\resources\images\*.png %CB_OUTPUT_RESDIR%\images > nul
-xcopy /D /y src\resources\images\settings\*.png %CB_OUTPUT_RESDIR%\images\settings > nul
-xcopy /D /y src\resources\images\16x16\*.png %CB_DEVEL_RESDIR%\images\16x16 > nul
-xcopy /D /y src\resources\images\16x16\*.png %CB_OUTPUT_RESDIR%\images\16x16 > nul
-xcopy /D /y src\resources\images\22x22\*.png %CB_DEVEL_RESDIR%\images\22x22 > nul
-xcopy /D /y src\resources\images\22x22\*.png %CB_OUTPUT_RESDIR%\images\22x22 > nul
-xcopy /D /y plugins\codecompletion\resources\images\*.png %CB_DEVEL_RESDIR%\images\codecompletion > nul
-xcopy /D /y plugins\codecompletion\resources\images\*.png %CB_OUTPUT_RESDIR%\images\codecompletion > nul
-xcopy /D /y plugins\compilergcc\resources\compilers\*.xml %CB_DEVEL_RESDIR%\compilers > nul
-xcopy /D /y plugins\compilergcc\resources\compilers\*.xml %CB_OUTPUT_RESDIR%\compilers > nul
-echo Makefile.am >  excludes30.txt
-echo Makefile.in >> excludes30.txt
-echo \.svn\      >> excludes30.txt
-xcopy /D /y /s plugins\scriptedwizard\resources\* %CB_DEVEL_RESDIR%\templates\wizard /EXCLUDE:excludes30.txt >nul
-xcopy /D /y /s plugins\scriptedwizard\resources\* %CB_OUTPUT_RESDIR%\templates\wizard /EXCLUDE:excludes30.txt >nul
-xcopy /D /y templates\common\* %CB_OUTPUT_RESDIR%\templates /EXCLUDE:excludes30.txt > nul
-xcopy /D /y templates\win32\* %CB_OUTPUT_RESDIR%\templates /EXCLUDE:excludes30.txt > nul
-xcopy /D /y templates\common\* %CB_DEVEL_RESDIR%\templates /EXCLUDE:excludes30.txt > nul
-xcopy /D /y templates\win32\* %CB_DEVEL_RESDIR%\templates /EXCLUDE:excludes30.txt > nul
-xcopy /D /y scripts\* %CB_DEVEL_RESDIR%\scripts /EXCLUDE:excludes30.txt > nul
-xcopy /D /y scripts\* %CB_OUTPUT_RESDIR%\scripts /EXCLUDE:excludes30.txt > nul
-xcopy /D /y scripts\tests\* %CB_DEVEL_RESDIR%\scripts\tests /EXCLUDE:excludes30.txt > nul
-xcopy /D /y scripts\tests\* %CB_OUTPUT_RESDIR%\scripts\tests /EXCLUDE:excludes30.txt > nul
-del excludes30.txt
-xcopy /D /y tips.txt %CB_DEVEL_RESDIR% > nul
-xcopy /D /y tips.txt %CB_OUTPUT_RESDIR% > nul
-echo Synching files between devel and output folder
-xcopy /D /y tools\ConsoleRunner\cb_console_runner.exe output30 > nul
-xcopy /D /y tools\ConsoleRunner\cb_console_runner.exe devel30 > nul
-xcopy /D /y devel30\*.exe output30 > nul
-xcopy /D /y devel30\*.dll output30 > nul
-xcopy /D /y %CB_DEVEL_RESDIR%\plugins\*.dll %CB_OUTPUT_RESDIR%\plugins > nul
-
-echo Stripping debug info from output30 tree
-strip output30\*.exe
-strip output30\*.dll
-strip %CB_OUTPUT_RESDIR%\plugins\*.dll
-
-REM Copy these files later as stripping symbols would corrupt them
-echo Copying crash handler files
-xcopy /y exchndl\win32\bin\*.dll devel30  > nul
-xcopy /y exchndl\win32\bin\*.yes devel30  > nul
-xcopy /y exchndl\win32\bin\*.dll output30 > nul
-xcopy /y exchndl\win32\bin\*.yes output30 > nul
+@call update.bat 30

commit 10cd56d0f1cfc32b5af75481416cf63ceabb8254

- build: Fix regression caused by r11652 (ticket #819, thanks Miguel Gimenez)
> Using find might fail if there is unix variant in path which is found
  before the windows version.

git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11653 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/update.bat
@@ -12,7 +12,9 @@ set CB_OUTPUT_DIR=output%1%
 set CB_DEVEL_RESDIR=%CB_DEVEL_DIR%\share\CodeBlocks
 set CB_OUTPUT_RESDIR=%CB_OUTPUT_DIR%\share\CodeBlocks
 set CB_HANDLER_DIR=exchndl\win32\bin
-set %i% | find "_64" > nul && set CB_HANDLER_DIR=exchndl\win64\bin
+set TARGET=%1%
+set TARGET_CUT=%TARGET:_64=%
+if NOT "%TARGET%" == "" if NOT "%TARGET%" == "%TARGET_CUT%" set CB_HANDLER_DIR=exchndl\win64\bin
 
 call:mkdirSilent %CB_DEVEL_RESDIR%\compilers
 call:mkdirSilent %CB_DEVEL_RESDIR%\lexers